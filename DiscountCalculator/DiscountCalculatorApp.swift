c46ad8f68c5fd0762a740252e53f73adcd686c33
Elaine Fang <efang@lcs.on.ca>
2025-01-20 09:43:45 -0500

    Initial Commit

---
+++ DiscountCalculator/DiscountCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscountCalculatorApp.swift
+//  DiscountCalculator
+//
+//  Created by Fang Elaine on 2025/1/20.
+//
+
+import SwiftUI
+
+@main
+struct DiscountCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
