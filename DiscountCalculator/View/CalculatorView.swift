bb785bec6a29dc4980accb6bc9830f66bd14097b
Elaine Fang <efang@lcs.on.ca>
2025-01-20 10:01:13 -0500

    Organized the files and created CalculatorView. (End of the first class)

---
+++ DiscountCalculator/View/CalculatorView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  DiscountCalculator
+//
+//  Created by Fang Elaine on 2025/1/20.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+   
+    @State var originalPrice = " "
+    
+    @State var discountPercentage : Double = 1
+    
+    @State var feedback = " "
+    
+}
+
+#Preview {
+    CalculatorView()
+}
